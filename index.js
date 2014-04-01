fc42c6a63f4ca45363cbfa5a8a61c63097471a95
az402 <tian1.fu@gmail.com>
2014-04-01 15:40:27 +0800

    test

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ app.use('/wechat', wechat(configs.token, wechat.text(function (info, req, res, n
       });
       response = list.length!=0?response+list.join("\r\n")+"\r\n回复数字查询药价。":"对不起未查询到治疗"+info.Content+"症状的药品。";
 
+      console.log(response);
+
       session.last = info.Content;
       session.last_list.push(list);
 
