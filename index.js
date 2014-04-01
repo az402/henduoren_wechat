e121c47b02344e1438e99639d3355d281c0578f6
az402 <tian1.fu@gmail.com>
2014-04-01 11:01:52 +0800

    test

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ app.use('/wechat', wechat(configs.token, wechat.text(function (info, req, res, n
         list[i++]=(i+"."+window.$(this).text().replace(/\s/g,"")).split("-")[0];
       });
       response = list.length!=0?response+list.join("\r\n")+"\r\n回复数字查询药价。":"对不起未查询到治疗"+info.Content+"症状的药品。";
+      req.wxsession.text = info.Content;
       res.reply(response);
       console.log("reply "+response)
-      req.wxsession.text = info.Content;
     });
   }
 })));
